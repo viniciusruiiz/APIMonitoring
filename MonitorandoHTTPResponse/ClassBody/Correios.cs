01fc048c052bedd5781844b5efbdc6102284ce80
vsruiz <vsruiz@stefanini.com>
2019-05-13 14:26:45 -0300

    Add project files.

---
+++ MonitorandoHTTPResponse/ClassBody/Correios.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitorandoHTTPResponse.ClassBody
+{
+    public class Correios
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string UF { get; set; }
+        public string Unidade { get; set; }
+        public string IBGE { get; set; }
+        public string Gia { get; set; }
+
+        private string ValidateResponse(string responseItem)
+        {
+            return responseItem.Equals(string.Empty) ? "Não registrado/Não existe" : responseItem;
+        }
+
+        public override string ToString()
+        {
+            Cep = ValidateResponse(Cep);
+            Logradouro = ValidateResponse(Logradouro);
+            Complemento = ValidateResponse(Complemento);
+            Bairro = ValidateResponse(Bairro);
+            Complemento = ValidateResponse(Complemento);
+            Bairro = ValidateResponse(Bairro);
+            Localidade = ValidateResponse(Localidade);
+            UF = ValidateResponse(UF);
+            Unidade = ValidateResponse(Unidade);
+            IBGE = ValidateResponse(IBGE);
+            Gia = ValidateResponse(Gia);
+
+            return string.Format("API: Correios\n\nCEP: {0}\nLogradouro: {1}\nComplemento: {2}\nBairro: {3}\nLocalidade: {4}\nUF: {5}\nUnidade: {6}\nIBGE: {7}\nGia: {8}",
+                Cep,
+                Logradouro,
+                Complemento,
+                Bairro,
+                Localidade,
+                UF,
+                Unidade,
+                IBGE,
+                Gia);
+        }
+    }
+}
---
+++ MonitorandoHTTPResponse/Monitoring.cs
@@ -0,0 +1,89 @@
+﻿using MonitorandoHTTPResponse.ClassBody;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitorandoHTTPResponse
+{
+    class Monitoring
+    {
+        private static bool _isOnline;
+        private static bool _isValidApi;
+
+        /// <summary>
+        /// Método Responsável por ler o status code da API, e validar se a API está ou não online.
+        /// </summary>
+        private static void ValidateStatusCode(HttpStatusCode code)
+        {
+            Console.WriteLine("validando codigo de resposta...");
+            _isOnline = code == HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Método Responsável por ler o corpo da API, e validar se a resposta é o esperado
+        /// </summary>
+        private static void ValidateResponse(string response)
+        {
+            Console.WriteLine("validando corpo de resposta...");
+
+            Correios api = JsonConvert.DeserializeObject<Correios>(response);
+
+            //Conversar sobre deixar específico ou genérico
+            _isValidApi = api.Cep == "03901-010" && api.Logradouro == "Avenida dos Nacionalistas";
+        }
+
+        static async Task Main()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response;
+
+                    while (true)
+                    {
+                        response = await client.GetAsync("https://viacep.com.br/ws/03901010/json/");//Faz a requisição
+
+                        ValidateStatusCode(response.StatusCode);//Valida o status code retornado da requisição
+
+                        if (_isOnline)
+                        {
+                            Console.WriteLine("API Online!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("API Offline :(");
+                        }
+
+                        ValidateResponse(await response.Content.ReadAsStringAsync());//Valida o corpo da requisição
+
+                        if (_isValidApi)
+                        {
+                            Console.WriteLine("Resposta da API validada!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Resposta da API ínvalida! :(");
+                        }
+
+                        Thread.Sleep(3000);//Tempo de delay de monitoramento, a validar.
+
+                        Console.WriteLine("");//Quebra de linha para o próximo monitoramento
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    //tratamento de exceção
+                    Console.WriteLine("\nOcorreu uma exceção!");
+                    Console.WriteLine("Mensagem :{0} ", e.Message);
+                }
+            }
+        }
+    }
+}
